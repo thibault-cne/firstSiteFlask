b4d30e173504da6a6683463523d1ee14cef5b0dc
Thibault Cheneviere <thibault.chene23@gmail.com>
2021-09-21 23:45:43 +0200

    Modification des champs dans les forms pour les rendres obligatoires. Retrait de la navBar pour en faire une perso.

--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -1,15 +1,31 @@
 // Fonction to validate if the user input is an email adress
 
+function ValidateForm() {
+  eMail = document.getElementById('mailInput').value;
+  userName = document.getElementById('userNameInput').value;
+  pwd = document.getElementById('pwdInput').value;
+  pwdConfirm = document.getElementById('pwdConfirmInput').value;
 
-function ValidateEmail(input) {
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (input.value.match(validRegex)) {
-      alert("Valid email address!");
-      document.form.eMail.focus();
-      return true;
-    } else {
-      alert("Votre adresse mail est invalide !\nMerci de réessayer avec une adresse valide.");
-      document.form.eMail.focus();
-      return false;
-    }
+  if (eMail=='') {
+    alert("Merci de rentrer une adresse mail !");
+    return false;
   }
+
+  if (userName='') {
+    alert("Merci de rentrer un nom d'utilisateur !");
+    return false;
+  }
+
+  if (pwd='' || pwd.length < 4 || pwd.length > 20 ) {
+    alert("Merci de rentrer un mot de passe entre 4 et 20 caractères !");
+    return false;
+  }
+
+  if (pwdConfirm='') {
+    alert("Merci de confirmer votre mot de passe !");
+    return false;
+  }
+
+  return true;
+    
+}
